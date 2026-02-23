100a33574603a50c9c0b798d5f54bedb2a59885b
yuxins0803-code <yuxins.0803@gmail.com>
2026-02-23 14:02:22 -0500

    Update BondDuration_File.py

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,3 +1,19 @@
 
 def getBondDuration(y, face, couponRate, m, ppy = 1):
-    return(8.51)
+    y_eff=y/ppy
+    c_eff=couponRate/ppy
+    m_eff=m*ppy
+
+    pvcfsum=0
+    t_pvcfsum=0
+
+    for t in range(1,m_eff+1):
+        cf=face*c_eff
+        if t==m_eff:
+            cf=cf+face
+        pvcf=cf/((1+y_eff)**t)
+        pvcfsum=pvcfsum+pvcf
+        t_pvcfsum=t_pvcfsum+(t/ppy)*pvcf
+
+    bondDuration=t_pvcfsum/pvcfsum
+    return bondDuration
