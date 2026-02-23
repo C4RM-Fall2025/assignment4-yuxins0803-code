54f9c46c7b64ee4779ec8faba943575116783ece
yuxins0803-code <yuxins.0803@gmail.com>
2026-02-23 14:14:58 -0500

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,4 +1,13 @@
 
 
 def getBondPrice_E(face, couponRate, yc):
-    return(2098949)
+    m=len(yc)
+    coupon=face*couponRate
+    bondPrice=0
+
+    for t in range(1,m+1):
+        cf=coupon
+        if t==m:
+            cf=cf+face
+        bondPrice=bondPrice+cf/((1+yc[t-1])**t)
+    return(bondPrice)
